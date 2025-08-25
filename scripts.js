93b615560bac6a7df5251267c3f76e9f45b502ad
Giovanni Villas <giovannivchaves@gmail.com>
2025-08-25 19:09:46 -0300

    home 100% finalizada, amém

---
+++ scripts.js
@@ -0,0 +1,47 @@
+const prevButton = document.getElementById("prev");
+const nexttButton = document.getElementById("next");
+const itens = document.querySelectorAll(".item");
+const pontos = document.querySelectorAll(".ponto");
+const numero = document.querySelector(".numero-pg");
+const list = document.querySelector(".itens-carrocel");
+
+let ativo = 0;
+const total = itens.length;
+let timer;
+
+function update(direction) {
+  document.querySelector(".item.ativo").classList.remove("ativo");
+  document.querySelector(".ponto.ativo").classList.remove("ativo");
+
+  if (direction > 0) {
+    ativo = ativo + 1;
+
+    if (ativo === total) {
+      ativo = 0;
+    }
+  } else if (direction < 0) {
+    ativo = ativo - 1;
+
+    if (ativo < 0) {
+      ativo = total - 1;
+    }
+  }
+
+  itens[ativo].classList.add("ativo");
+  pontos[ativo].classList.add("ativo");
+
+  numero.textContent = ativo + 1;
+}
+
+clearInterval(timer);
+timer = setInterval(() => {
+  update(1);
+}, 5000);
+
+prevButton.addEventListener("click", function () {
+  update(-1);
+});
+
+nexttButton.addEventListener("click", function () {
+  update(1);
+});
